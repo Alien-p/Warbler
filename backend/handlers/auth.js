1e96eb41fe7a4acc6ebe817fad2b8272f941faeb
Ildar Gumerov <Ildar.Gumerov@icl-services.com>
2020-02-27 15:41:48 +0300

    Add signin rout

--- backend/handlers/auth.js
+++ backend/handlers/auth.js
@@ -1,20 +1,29 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.signin = function() {}
-
-exports.getUser = async function(req, res, next) {
-  try {
-    let users = await db.User.find();
-    return res.status(200).json({users});
-  } catch (err) {
-    let error = {
-      status: 400,
-      message: err.message
+exports.signin = async function(req, res, next) {
+  try{
+    let user = await db.User.findOne({email: req.body.email});
+    let {id, username, profileImageUrl} = user;
+    let isPassMatch = await user.comparePassword(req.body.password);
+    
+    if(isPassMatch) {
+      let token = jwt.sign({ id, username, profileImageUrl }, process.env.SECRET_KEY);
+      return res.status(200).json({ id, username, profileImageUrl, token });
+    } else {
+      return next({
+        status:400,
+        message: 'Invalid Email or Password'
+      })
     }
-    return next(error);
+  } catch (err) {
+    return next({
+      status:400,
+      message: 'Invalid Email or Password'
+    })
   }
 }
+
 
 exports.signup = async function(req, res, next) {
   try {
@@ -24,14 +33,25 @@ exports.signup = async function(req, res, next) {
     let {id, username, profileImageUrl} = user;
     console.log(process.env.SECRET_KEY);
     let token = jwt.sign({ id, username, profileImageUrl }, process.env.SECRET_KEY);
-    debugger;
     return res.status(200).json({id, username, profileImageUrl, token})
   } catch (err) {
     //what kind of error
-    debugger;
     if(err.code === 11000) {
       err.message = "Sorry, that username/email is already taken"; 
     }
+    let error = {
+      status: 400,
+      message: err.message
+    }
+    return next(error);
+  }
+}
+
+exports.getUser = async function(req, res, next) {
+  try {
+    let users = await db.User.find();
+    return res.status(200).json({users});
+  } catch (err) {
     let error = {
       status: 400,
       message: err.message
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {signup, getUser} = require('../handlers/auth');
+const {signup, signin, getUser} = require('../handlers/auth');
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 router.get('/users', getUser);
 
 module.exports = router;
